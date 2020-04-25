0930081be41b237c7eb5d6c5cc6d14bbf6503cb1
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-04-25 10:47:29 -0400

    Begin component design for newsfeed view. Focus on working Typescript.

--- angular-development-0A/firing-squad-heros/src/app/app.component.ts
+++ angular-development-0A/firing-squad-heros/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'firing-squad-heros';
+  title = 'Firing Squad Network';
+  newsfeed = 'Newsfeed View';
 }
