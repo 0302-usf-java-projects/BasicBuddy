f8cea03c66ddd73602e6b4049cbbf6e9d85f8eb1
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-04-25 14:57:52 -0400

    Component simple declaration works

--- angular-development-0A/firing-squad-heros/src/app/app.module.ts
+++ angular-development-0A/firing-squad-heros/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { BuddiesComponent } from './buddies/buddies.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsfeedComponent,
+    BuddiesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-development-0A/firing-squad-heros/src/app/buddies/buddies.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buddies',
+  templateUrl: './buddies.component.html',
+  styleUrls: ['./buddies.component.css']
+})
+export class BuddiesComponent implements OnInit {
+
+  buddy = 'Buddy';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
