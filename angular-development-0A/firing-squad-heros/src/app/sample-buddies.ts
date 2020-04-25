20b8cadaf09070915199f7e7f4ef4a13e584a2e7
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-04-25 15:56:59 -0400

    refactor component

---
+++ angular-development-0A/firing-squad-heros/src/app/sample-buddies.ts
@@ -0,0 +1,11 @@
+import { Buddy } from './hero';
+
+export const BUDDIES: Buddy[] = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Calladocious' },
+  { id: 4, name: 'Dave' },
+  { id: 5, name: 'Evan' },
+  { id: 6, name: 'Francine' },
+  { id: 7, name: 'Gertrude' },
+];
